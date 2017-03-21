9bd3b2274dce03e7716ad9277c277a8b108bde6a
root <root@anykey.abills.net>
2017-03-21 10:16:46 +0200

    Import start files

--- lng_ukraine.pl
+++ lng_ukraine.pl
@@ -4,5 +4,6 @@ $lang{APARTMENT_AREA}='Загальна площа квартири';
 $lang{COMMERCIAL}='Комерційні приміщення';
 $lang{OSBB}='ОСББ';
 $lang{RENT}='Квартплата';
+$lang{FILL_DEFAULT_VALUES_AND_CHOOSE_FILE_FOR_IMPORT} = 'Заповніть значення однакові для всіх жильців та виберіть файл для імпорту';
 
 1;
