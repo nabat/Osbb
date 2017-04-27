7e55cfaa3a75acfdb154b517f1a543e90ca47553
Anykey <antoman1994@gmail.com>
2017-04-27 10:04:03 +0300

    Osbb. sq realogned

--- Osbb.sql
+++ Osbb.sql
@@ -4,21 +4,20 @@ CREATE TABLE IF NOT EXISTS `osbb_ownership_types` (
   PRIMARY KEY (`id`)
 ) COMMENT = 'Osbb ownership types';
 
-REPLACE INTO `osbb_ownership_types` (`id`,`name`) VALUES
+REPLACE INTO `osbb_ownership_types` (`id`, `name`) VALUES
   (1, 'Приватизована'),
   (2, 'Комунальна'),
   (3, 'Відомча'),
   (4, 'Державна (бронь, без врахування надлишків)'),
   (5, 'Викуплена'),
   (6, 'Кооперативна'),
-  (7, '-')
-;
+  (7, '-');
 
 CREATE TABLE IF NOT EXISTS `osbb_main` (
   `uid` INT(10) UNSIGNED NOT NULL,
   `type` TINYINT(3) UNSIGNED NOT NULL DEFAULT '0',
   `ownership_type` TINYINT(3) UNSIGNED REFERENCES `osbb_ownership_types` (`id`) ON DELETE RESTRICT,
-  `total_area` DOUBLE(10,2) UNSIGNED NOT NULL DEFAULT 0.00,
+  `total_area` DOUBLE(10, 2) UNSIGNED NOT NULL DEFAULT 0.00,
   `living_area` DOUBLE(10, 2) UNSIGNED NOT NULL DEFAULT 0.00,
   `utility_area` DOUBLE(10, 2) UNSIGNED NOT NULL DEFAULT 0.00,
   `balcony_area` DOUBLE(10, 2) UNSIGNED NOT NULL DEFAULT 0.00,
@@ -26,7 +25,8 @@ CREATE TABLE IF NOT EXISTS `osbb_main` (
   `rooms_count` TINYINT(2) UNSIGNED NOT NULL DEFAULT 1,
   `people_count` TINYINT(2) UNSIGNED NOT NULL DEFAULT 1,
   PRIMARY KEY (`uid`)
-) COMMENT = 'OSBB users';
+)
+  COMMENT = 'OSBB users';
 
 
 CREATE TABLE IF NOT EXISTS `osbb_area_types` (
@@ -38,33 +38,37 @@ CREATE TABLE IF NOT EXISTS `osbb_area_types` (
   `domain_id` SMALLINT(3) UNSIGNED NOT NULL DEFAULT '0',
   `comments` TEXT,
   UNIQUE KEY (`name`)
-) COMMENT = 'OSBB area types';
+)
+  COMMENT = 'OSBB area types';
 
 CREATE TABLE IF NOT EXISTS `osbb_spending_types` (
   `id` INT(2) UNSIGNED NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(200) NOT NULL DEFAULT '',
   `comments` TEXT,
   PRIMARY KEY (`id`)
-) COMMENT = 'OSBB spending types';
+)
+  COMMENT = 'OSBB spending types';
 
 CREATE TABLE IF NOT EXISTS `osbb_tarifs` (
-  `id`            smallint(5)  unsigned NOT NULL AUTO_INCREMENT,
-  `name`          varchar(40)           NOT NULL DEFAULT '',
-  `unit`          tinyint(1)            NOT NULL DEFAULT '0',
-  `price`         double(10,2) unsigned NOT NULL DEFAULT '0.00',
-  `document_base` varchar(120)          NOT NULL DEFAULT '',
-  `start_date`    DATE                  NOT NULL,
-  `set_all`       tinyint(1)   UNSIGNED NOT NULL DEFAULT '0',
+  `id` SMALLINT(5) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(40) NOT NULL DEFAULT '',
+  `unit` TINYINT(1) NOT NULL DEFAULT '0',
+  `price` DOUBLE(10, 2) UNSIGNED NOT NULL DEFAULT '0.00',
+  `document_base` VARCHAR(120) NOT NULL DEFAULT '',
+  `start_date` DATE NOT NULL,
+  `set_all` TINYINT(1) UNSIGNED NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`),
   UNIQUE KEY `id` (`id`),
   UNIQUE KEY `name` (`name`)
-  ) COMMENT = 'Osbb Tarifs';
-  
-  CREATE TABLE IF NOT EXISTS `osbb_users_services` (
-  `id`            SMALLINT(5)  UNSIGNED NOT NULL AUTO_INCREMENT,
-  `uid`           INT(10)      UNSIGNED NOT NULL,
-  `tp_id`         SMALLINT(5)  UNSIGNED NOT NULL,
+)
+  COMMENT = 'Osbb Tarifs';
+
+CREATE TABLE IF NOT EXISTS `osbb_users_services` (
+  `id` SMALLINT(5) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `uid` INT(10) UNSIGNED NOT NULL,
+  `tp_id` SMALLINT(5) UNSIGNED NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id` (`id`),
-  UNIQUE KEY `uid_tp` (`uid`,`tp_id`)
-  ) COMMENT = 'Osbb Users Services';
+  UNIQUE KEY `uid_tp` (`uid`, `tp_id`)
+)
+  COMMENT = 'Osbb Users Services';
